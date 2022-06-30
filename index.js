f94cb04f22d9a8330b9e4331770943025824295f
Julian Heaney <julianpheaney@gmail.com>
2022-06-30 01:54:39 -0500

    initial work first two prompts

---
+++ index.js
@@ -0,0 +1,18 @@
+import inquirer from 'inquirer';
+inquirer
+    .prompt([ {
+        name: 'title',
+        message: 'What is the Title of Your Project?',
+        type: 'input' 
+    },
+    {
+        name: 'description',
+        message: 'What Does Your Project Do?',
+        type: 'input'
+
+    }])
+    .then((answers) => {
+        console.log("#" + answers["title"] + "\r\n");
+        console.log(answers["description"]);
+    });
+    
